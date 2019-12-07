415427c312ef34fbf0799888fec794781069efc6
Yatharth2049 <51695136+Yatharth2049@users.noreply.github.com>
2019-12-07 23:07:51 +0530

    Update nasa_apod.py

--- nasa_apod.py
+++ nasa_apod.py
@@ -1,4 +1,5 @@
 from flask import Flask, escape, request, render_template
+from pyPdf import PdfFileWriter, PdfFileReader
 import requests
 app = Flask(__name__)
 
@@ -11,19 +12,22 @@ def myform():
 
 def apod():
         render_template('html_file.html')
-        akey=open(api_key)
-        key=akey.read()
+        #akey=open(api_key)
+        key='8bj3Let2hyQgKRVCG7qfTtMQzEIJeFfPHde2Vra4'#akey.read()
         year = request.form['year']
         month = request.form['month']
         day = request.form['day']
         url=f'https://api.nasa.gov/planetary/apod?date={year}-{month}-{day}&api_key={key}'
         response=requests.get(url, allow_redirects=True)
+        open("filename.jpg","wb").write(response.content)#filename is the file in which the image is.
+        output_img = PdfFileWriter()
         if (response.status_code) == 404:
                 print(response.status_code)
         download = input('Download as pdf')
         if download == True:
-                input = PdfFileReader(open(filename,"rb"))#filename is the file in which the image is.
-                output.addPage(input.getPage(1))
+                file = PdfFileReader(open(filename,"rb"))#filename is the file in which the image is.
+                for i in range(0,file.getNumPages()):
+                            output_img.addPage(file.getPage(i))
 if __name__ == '__main__':
     app.run()
         
